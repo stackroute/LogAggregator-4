6f078e4665c67ab735c53f5848f2f87494f2c3d6
rahildar <postrahil@gmail.com>
2016-03-19 22:58:33 +0530

    executing expression on stream of data and modifying the stream

--- filter.js
+++ filter.js
@@ -3,108 +3,26 @@ var _=require('highland');
 var readableStream = fs.createReadStream('logs.json');
 readableStream.setEncoding('utf8');
 var keys=['insertions','deletions','authorName','authorEmail'];
-var exp="select insertions deletions authorName authorEmail from StreamA if(insertions>max(rollingCount(1min,insertions)))"
-var expkeys;
-var aggregates=['average','min','max'];
-var accumulators=['rollingCount','rollingTime'];
-var aggregateVariables=[];
-var patterns=[];
-for (var j = 0; j < aggregates.length; j++) {
-  for (var i = 0; i < accumulators.length; i++) {
-    patterns.push('/'+aggregates[j]+'\('+accumulators[i]+'/')
-  }
-}
+//var exp="select record.insertions deletions authorName authorEmail from StreamA if(insertions>max(rollingCount(1min,insertions)))"
+var exp="select record.insertions deletions authorName authorEmail from StreamA if(insertions>deletions)"
 
-
-
-
-
-
-
-var conditions =exp.match(/if\(.+[><=(!=)(contains)][(avesrage)(max)(min)]\)/g)
-conditions=conditions.toString().split(" ");
-for (var i = 0; i < conditions.length; i++) {
-  for (var i = 0; i < keys.length; i++) {
-    if(conditions.indexOf(keys[i])>=0)
-    aggregateVariables.push(keys[i]);
-  }
-}
-var logicaloperators=[];
-var operators=['>','<','=','!==','contains'];
-for (var i = 0; i < conditions.length; i++) {
-  if(conditions[i].indexOf('>')>=0){
-  logicaloperators.push('>')
-  }
-  else if (conditions[i].indexOf('<')>=0) {
-    logicaloperators.push('<')
-  }
-  else if (conditions[i].indexOf('=')>=0) {
-    logicaloperators.push('=')
-  }
-  else if (conditions[i].indexOf('!=')>=0) {
-    logicaloperators.push('!=')
-  }
-  else if (conditions[i].indexOf('contains')>=0) {
-    logicaloperators.push('!=')
-  }
-}
-for (var j = 0; j < conditions.length; j++) {
-  var code='';
-  for (var i = 0; i < logicaloperators.length; i++) {
-    conditions[j].split(logicaloperators[i]).each(function(singlecondition) {
-      for (var k = 0; k < patterns.length; k++) {
-        singlecondition.match(patterns[k]);
-        code+=k;
-      }
+var condition =exp.match(/if\(.+[><=(!=)(contains)][(avesrage)(max)(min)]\)/g)
+condition=condition.toString();
+condition="if((record.insertions>record.deletions) || record.type=='commit' )";
+var through = _.pipeline(
+        _.map(function (chunk) {
+        var data=JSON.parse(chunk);   // assuming the chunk/stream to be array of objects
+        data.map(function(record) {
+        var bool='false';
+        bool=eval(condition+"true");
+        if(bool)
+        record.highlight=true;
+        })
+        return JSON.stringify(data);
+    }),
+    _.filter(function (chunk) {
+//        console.log("index of all highlights"+);
+      return chunk
     })
-  }
-}
-
-
-
-
-
-
-// console.log(logicaloperators);
-
-
-
-
-
-
-
-// var through = _.pipeline(
-//     _.map(function (chunk) {
-//         var data=JSON.parse(chunk);   // assuming the chunk/stream to be array of objects
-//         if(!keys)
-//         keys=Object.keys(data[0]);    // All the keys of a single record
-//         if(!expkeys)
-//         {
-//             var temp=exp.split(" ");
-//             if(temp[1]=='*');
-//             {
-//                 expkeys=keys;        //keys in expression
-//             }
-//             else{
-//                 var i=1;
-//                 while(temp[i]!=='from')
-//                 expkeys.push(temp[i])
-//             }
-//         }
-//         }
-//
-//
-//         // if(data[0].insertions > 100)
-//         // {
-//         //     data[0].highlight=true;
-//         // }
-//         // else{
-//         //     data[0].highlight=false;
-//         // }
-//         return JSON.stringify(data);
-//     }),
-//     _.filter(function (chunk) {
-//       console.log("hello");
-//     })
-//     );
-// _(readableStream).pipe(through).pipe(process.stdout);
+    );
+_(readableStream).pipe(through).pipe(process.stdout);
