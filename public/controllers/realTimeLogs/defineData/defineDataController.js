c7cc28cf91bcd06eb2fb25d958151d38e9612497
shefalivaish09 <shefalivaish09@mail.com>
2016-04-01 15:19:08 +0530

    Prototype UI for define data updated : Second commit

--- public/controllers/realTimeLogs/defineData/defineDataController.js
+++ public/controllers/realTimeLogs/defineData/defineDataController.js
@@ -1,6 +1,5 @@
 angular.module('logAggregator').controller('defineDataController', ['$scope',
 function($scope) {
-
 console.log("inside define data controller");
   // $scope.$parent.clicked="aboutus";
 
