6a4db2ffdf270db5e9a0e590236666e3b071d37b
shefalivaish09 <shefalivaish09@mail.com>
2016-04-01 11:59:15 +0530

    updated files to add defineData feauturePage:First Commit

---
+++ public/controllers/realTimeLogs/defineData/defineDataController.js
@@ -0,0 +1,7 @@
+angular.module('logAggregator').controller('defineDataController', ['$scope',
+function($scope) {
+
+console.log("inside define data controller");
+  // $scope.$parent.clicked="aboutus";
+
+}]);
