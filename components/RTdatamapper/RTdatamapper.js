47a64add68065be70f62b4a8860fae6a9e16d16c
shefalivaish09 <shefalivaish09@mail.com>
2016-04-11 16:34:38 +0530

    updated Integrated real time data mapper code

--- components/RTdatamapper/RTdatamapper.js
+++ components/RTdatamapper/RTdatamapper.js
@@ -1,24 +1,15 @@
-// 17/3/16 mongo client
 
-// #2
 var _ = require('highland');
-var fs = require('fs')
-  //  var uri  = 'mongodb://localhost:27017/slog4'
 var WebSocketClient = require('websocket').client;
 var WebSocket1 = new WebSocketClient();
-
-
-var WebSocketServer = require('ws').Server
-  //  , http = require('http')
-  ,
-  express = require('express'),
-  app = express.createServer();
-
-app.use(express.static(__dirname + '/public'));
-app.listen(9090);
-
-var wss = new WebSocketServer({
-  server: app
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({port: 9090});
+var serverWs;
+wss.on('connection', function(ws) {
+  console.log('CONNECTED');
+  ws.send('Connected');
+  serverWs = ws;
+  ws.setMaxListeners(ws.getMaxListeners() + 1);
 });
 
 var measures = [{
@@ -38,7 +29,6 @@ WebSocket1.on('connect', function(connection) {
   console.log("Connected..Waiting for some message");
   var streamData = {};
   _('message', connection).map(function(msg) {
-    wss.on('connection', function(ws) {
       streamData = JSON.parse(msg.utf8Data)[2];
       var keys = Object.keys(streamData); //array of keys in the streaming data
       for (var k = 0; k < measures.length; k++) {
@@ -55,24 +45,16 @@ WebSocket1.on('connect', function(connection) {
           }
         }
       }
-      ws.send(JSON.stringify(streamData));
-      ws.on('close', function() {
-        //console.log('stopping client interval');
-        //clearInterval(id);
-      });
-      //
-      ws.setMaxListeners(ws.getMaxListeners() + 1);
-    });
-
-    streamData = JSON.parse(msg.utf8Data)[2];
+      if(serverWs) {
+        serverWs.send(JSON.stringify(streamData));
+      }
     console.log(streamData);
   }).done(function() {
     console.log('Done');
   });
-  //
   connection.setMaxListeners(connection.getMaxListeners() + 1);
 });
 WebSocket1.connect('ws://172.23.238.253:7070');
-
-// function msg
-/* ending of program*/
+//
+// // function msg
+// /* ending of program*/
