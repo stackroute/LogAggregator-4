d90eb3bd87b5e3b768edc586bdb509959f69d5df
shefalivaish09 <shefalivaish09@mail.com>
2016-04-11 17:46:52 +0530

    Added post save hook on namespace update

--- models/namespaceSchema.js
+++ models/namespaceSchema.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var RTdatamapper = require('../components/RTdatamapper/RTdatamapper');
 
 var namespaceSchema = new mongoose.Schema({
   name:String,
@@ -33,7 +34,17 @@ namespaceSchema.statics.findNamespace=function(name,cb){
     });
   };
 
+  namespaceSchema.post('save',function (namespace) {
+      // if (!err){
+      console.log('nid' +namespace._id + '||' +namespace.measures);
+        RTdatamapper(namespace._id,namespace.measures);
 
+      //  log.info('post saving...', this);
+      // }
+      // else{
+      //     console.log('error occured in hook' + err);
+      // }
+    });
 
 
 module.exports=namespaceSchema;
