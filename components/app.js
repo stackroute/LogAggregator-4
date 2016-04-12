5e41a9279b1954d00b8dad0683af209130f181c2
neel0711 <neel.sinha26@gmail.com>
2016-04-12 17:42:00 +0530

    Added accumulator in components

---
+++ components/app.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var formidable = require('formidable');
+var session = require('express-session');
+
+var routes = require('./routes/index');
+var dimensionRouter = require('./routes/dimensions');
+var measuresRouter = require('./routes/measures');
+var namespaceRouter = require('./routes/logdata');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+app.use(session({
+  saveUninitialized: true,
+  resave: true,
+  secret: 'secret',
+  cookie: {
+    login: false
+  }
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.use('/', routes);
+app.use('/dimensions', dimensionRouter);
+app.use('/measures', measuresRouter);
+app.use('/logdata', namespaceRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    // res.render('error');
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  // res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
